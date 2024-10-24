42b24283465a25c2aa788be9549f1c1717dd7c9c
Abhijeet Katore <65213016+akatore@users.noreply.github.com>
2024-10-24 23:02:17 +0530

    Update providers.tf

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -13,7 +13,7 @@ provider "google" {
 }
 
 provider "kubernetes" {
-  host = google_container_cluster.default  # we can fine our host google_container_cluster.default because we have created, we need endpoint of our cluster
+  host = google_container_cluster.default.endpoint  # we can fine our host google_container_cluster.default because we have created, we need endpoint of our cluster
   token = data.google_client_config.current # we can get token from google_client_config
   client_certificate = base64decode(google_container_cluster.default.master_auth[0].client_certificate)
   client_key = base64decode(google_container_cluster.default.master_auth[0].client_key)
